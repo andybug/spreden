0e58174eaeb9010b392be6775ad92ee24652b434
Andrew Fields <andybug10@gmail.com>
2015-11-26 17:07:29 -0600

    Process verbose and version arguments

--- src/main.c
+++ src/main.c
@@ -1,14 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
 
 #include "spreden.h"
 
+enum spreden_options {
+	SPREDEN_OPTION_VERBOSE,
+	SPREDEN_OPTION_VERSION
+};
+
+static void display_version(void)
+{
+	printf("spreden version %d.%d\n",
+	       SPREDEN_VERSION_MAJOR,
+	       SPREDEN_VERSION_MINOR);
+}
+
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	int c;
+	int index;
+	struct spreden_state state;
+	bool version;
+	static const struct option options[] = {
+		{ "verbose", no_argument, NULL, SPREDEN_OPTION_VERBOSE },
+		{ "version", no_argument, NULL, SPREDEN_OPTION_VERSION },
+		{ NULL, 0, 0, 0 }
+	};
 
-	printf("spreden %d.%d\n", SPREDEN_VERSION_MAJOR, SPREDEN_VERSION_MINOR);
+	while ((c = getopt_long(argc, argv, "", options, &index)) != -1) {
+		switch (c) {
+		case SPREDEN_OPTION_VERBOSE:
+			state.verbose = true;
+			break;
+
+		case SPREDEN_OPTION_VERSION:
+			version = true;
+			break;
+		}
+	}
+
+	if (version) {
+		display_version();
+		return EXIT_SUCCESS;
+	}
 
 	return EXIT_SUCCESS;
 }
--- src/spreden.h
+++ src/spreden.h
@@ -1,7 +1,13 @@
 #ifndef SPREDEN_H
 #define SPREDEN_H
 
+#include <stdbool.h>
+
 #define SPREDEN_VERSION_MAJOR 0
 #define SPREDEN_VERSION_MINOR 1
+
+struct spreden_state {
+	bool verbose;
+};
 
 #endif
