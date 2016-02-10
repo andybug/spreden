c14775e6bc9e9f2302bc9ff5102fc26eef2fa175
Andrew Fields <andybug10@gmail.com>
2016-02-09 21:37:24 -0600

    Define week structure

--- src/db.c
+++ src/db.c
@@ -10,12 +10,21 @@
 
 #define DB_MAX_TEAMS    256
 #define DB_MAX_GAMES  32768
+#define DB_MAX_WEEKS    512
+
+struct week {
+	struct week_id id;
+	int game_begin;
+	int game_end;
+};
 
 struct db {
 	struct team teams[DB_MAX_TEAMS];
-	unsigned int num_teams;
 	struct game games[DB_MAX_GAMES];
+	struct week weeks[DB_MAX_WEEKS];
+	unsigned int num_teams;
 	unsigned int num_games;
+	unsigned int num_weeks;
 };
 
 
@@ -23,12 +32,17 @@ static void db_print_sizes(const struct db *db)
 {
 	fprintf(stderr, "db: DB_MAX_TEAMS = %u\n", DB_MAX_TEAMS);
 	fprintf(stderr, "db: DB_MAX_GAMES = %u\n", DB_MAX_GAMES);
+	fprintf(stderr, "db: DB_MAX_WEEKS = %u\n", DB_MAX_WEEKS);
 	fprintf(stderr, "db: sizeof(team) = %lu\n", sizeof(struct team));
 	fprintf(stderr, "db: sizeof(game) = %lu\n", sizeof(struct game));
+	fprintf(stderr, "db: sizeof(week) = %lu\n", sizeof(struct week));
 	fprintf(stderr, "db: total alloc teams = %lu\n",
 		sizeof(struct team) * DB_MAX_TEAMS);
 	fprintf(stderr, "db: total alloc games = %lu\n",
 		sizeof(struct game) * DB_MAX_GAMES);
+	fprintf(stderr, "db: total alloc weeks = %lu\n",
+		sizeof(struct week) * DB_MAX_WEEKS);
+	fprintf(stderr, "db: total alloc db    = %lu\n", sizeof(struct db));
 }
 
 static int db_init(struct state *s)
@@ -47,6 +61,7 @@ static int db_init(struct state *s)
 	/* init members */
 	db->num_teams = 0;
 	db->num_games = 0;
+	db->num_weeks = 0;
 
 	/* finally, set the db pointer in the state */
 	s->db = db;
